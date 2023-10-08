fc7db9b74f034692b24287fac5dbb182479ebe84
kmvalerio2 <krisvalerio02@gmail.com>
2023-10-07 22:37:35 -0600

    Added basic HTML, static folder for css, and currently adding SQL database

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 # 'virtualenv env', env is the conventional name to name your environment
 # To activate the environment, type 'source env/bin/activate'
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column()
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
